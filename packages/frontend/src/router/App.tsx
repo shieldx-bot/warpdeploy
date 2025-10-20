9833d2f8921f7ea3f8e59addcc86d4ff85bcdde7
Nguyen Van Anh <anhnv.24810310060@epu.edu.vn>
2025-10-21 00:46:09 +0700

    Refactor frontend structure and update configs
    Moved App.tsx to router/App.tsx and updated imports accordingly. Overhauled index.css to use Tailwind and custom CSS variables for theming. Added a .env file for CLIENT_ID_GITHUB. Updated tsconfig.json for stricter TypeScript settings and path aliases. Integrated Tailwind CSS plugin into Vite config.

--- packages/frontend/src/router/App.tsx
+++ packages/frontend/src/router/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +9,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -25,7 +23,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs ">
         Click on the Vite and React logos to learn more
       </p>
     </>
