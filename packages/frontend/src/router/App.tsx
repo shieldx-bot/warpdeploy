800703ee5280ce2544895dd068f812630ad2a09d
Nguyen Van Anh <anhnv.24810310060@epu.edu.vn>
2025-10-24 15:42:45 +0700

    Add login, signup, and OTP routes to App
    Added routes for LoginPage, SignupPage, and OTPPage to the App router. Updated import paths to use alias for consistency.

--- packages/frontend/src/router/App.tsx
+++ packages/frontend/src/router/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';  
-import Dashboard from '../components/dashboard/index';
 import { Route, Routes } from 'react-router-dom';
-
+import LoginPage from '@/components/login/index';
+import Dashboard from '@/components/dashboard/index';
+import SignupPage from '@/components/signup';
+import OTPPage from '@/components/otp';
+ 
 function App() {
    return (
     <div className='App'>
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>}  />
+        <Route path="/login" element={<LoginPage/>}  />
+        <Route path="/signup" element={<SignupPage/>}  />
+        <Route path="/otp" element={<OTPPage/>}  />
       </Routes>
     </div>
   )
