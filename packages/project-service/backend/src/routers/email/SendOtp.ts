72a7ada7c98cdf9a5ed03301bea93b9e220cc43c
Nguyen Van Anh <anhnv.24810310060@epu.edu.vn>
2025-10-25 11:47:14 +0700

    Add OTP email sending route and mailer utility
    Introduces a new Express route '/send-otp' for sending OTP codes via email using Gmail OAuth2 and nodemailer. Stores sent OTPs in the VerifyOtp database table and provides a sendMail utility function for email delivery.

---
+++ packages/project-service/backend/src/routers/email/SendOtp.ts
@@ -0,0 +1,80 @@
+import nodemailer, { type TransportOptions } from 'nodemailer';
+import { google } from 'googleapis';
+import { type Request, type Response } from 'express';
+import express from 'express';
+import { pool } from '../../database/config.js';
+import dotenv from 'dotenv';
+import authenticateToken from '../../middleware/auth.js';
+dotenv.config();
+const router = express.Router();
+const db = pool as any;
+const oAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URI
+)
+
+
+oAuth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+})
+
+
+
+
+router.post(
+    '/send-otp',  async(req: Request, res: Response) => { 
+        const {email} = req.body;
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        try { 
+            await sendMail(email, otp);
+            const sql = `
+            insert into VerifyOtp(email, otp ) values ($1, $2)
+            `;
+            await db.query(sql, [email, otp]);
+            res.status(200).json({status: 'success', message: 'OTP sent successfully' });
+        } catch (error) {
+            console.log("Error sending OTP email:", error);
+            res.status(500).json({ status: 'error', message: 'Failed to send OTP' });
+        }
+    }
+)
+
+
+
+
+
+
+async function sendMail(email: string, otp: string) {
+    try {
+        const acccessToken = await oAuth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAth2',
+                user: process.env.EMAIL_ADDRESS,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: acccessToken.token,
+            }
+        } as TransportOptions);
+
+        const mailOptions = {
+            from: `Bạn <${process.env.EMAIL_ADDRESS}>`,
+            to: `${email}`,
+            subject: "Test gửi Gmail qua OAuth2",
+            text: ` Mã OTP của bạn là: ${otp}`,
+            html: "<b>Hello world!</b>",
+        };
+
+
+    const result = await transporter.sendMail(mailOptions);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+export { router, sendMail };
