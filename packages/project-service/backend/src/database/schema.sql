06eb8f925b5290b66ea71a693330b4eec9e38780
Nguyen Van Anh <anhnv.24810310060@epu.edu.vn>
2025-10-31 23:16:10 +0700

    Revise schema and add server monitoring tables
    Updated SQL schema for compatibility with SQL Server, refactored table and column names, and improved foreign key constraints. Added new tables for server monitoring: info_server, cpu_server, memory_server, disk_server, net_server, and logged_server, including relevant indexes for health status. These changes support enhanced infrastructure monitoring and management features.

--- packages/project-service/backend/src/database/schema.sql
+++ packages/project-service/backend/src/database/schema.sql
@@ -1,90 +1,188 @@
-use master
+-- Active: 1761362626604@@127.0.0.1@1433@warm_deploy@dbo
+use warm_deploy
 go
 
 
-create  table if  not exists VerifyOtp ( 
-    id int primary key auto_increment,
+
+ 
+
+create  table  VerifyOtp ( 
+    id int primary key identity(1,1),
     email varchar(255) not null, 
     otp_code varchar(6) not null,
     created_at datetime  default current_timestamp,
-    expires_at datetime default (date_add(current_timestamp,interval 10 minute)),
-    used boolean default false,
-    index (email)
-)
+    expires_at AS DATEADD(MINUTE, 10, created_at) PERSISTED,
+    used bit default 0,
   
-
-go
-create table if not exists Users ( 
-    email  varchar(255) primary key, 
-    username varchar not null
+);
+create table Users ( 
+    email_user  varchar(255) primary key, 
+    username varchar not null,
     password_hash varchar(255) not null,
-    role char(30) default 'users'
+    role_at char(30) default 'users',
     created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp on update current_timestamp, 
+    updated_at datetime default current_timestamp, 
     github_username varchar(255),
     github_token varchar(255),
-    index(email, github_username)
-)
+     
+);
+
 go
-create table if not exists ConnectRepos ( 
-    id int primary key auto_increment,
+create table ConnectRepos ( 
+    id_cr int primary key identity(1,1),
     repo_name varchar(255) not null,
     repo_full_name varchar(255) not null,
     repo_url varchar(255) not null,
     branch_name varchar(255) not null,
     created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp on update current_timestamp,
-    unique key unique_repo (user_email, repo_full_name),
-    index (user_email, repo_full_name),
-    user_email varchar(255) constrain fk_user_email references USers(email),
+    updated_at datetime default current_timestamp ,
+    email_user varchar(255) foreign key references Users(email_user),
 )
 go
-create table if not exists deployments ( 
-    id int primary key auto_increment,
+create table deployments ( 
+    id_d int primary key identity(1,1 ) ,
     deployment_name varchar(255) not null,
     repo_id int not null,
     status varchar(50) not null,
     created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp on update current_timestamp,
-    unique key unique_deployment (deployment_name, repo_id),
-    index (deployment_name, repo_id),
-    repo_id int constrain fk_repo_id references ConnectRepos(id),
-    user_email constrain fk_user_email  references Users(email),
+    updated_at datetime default current_timestamp ,
+    id_cr int foreign key references ConnectRepos( id_cr),
+    email_user varchar(255) foreign key  references Users(email_user),
 )
 go
-create table if not exists deployment_logs (
-    id int primary key auto_increment, 
+create table deployment_logs (
+    id_dl int primary key identity, 
     log_message text not null,
     log_level varchar(50) not null,
     time_stamp datetime default current_timestamp,
-    deployment_id int constrain fk_deployment_id references deployments(id),
+    id_d int foreign key references deployments(id_d),
 )
 go 
-create table if not exists deployment_config ( 
-    id int primary key auto_increment,
-    ram int not null;
-    cpu in not null;
-    deployment_id int constrain fk_deployment_id references deployments(id),
+create table deployment_config ( 
+    id_dc int primary key identity(1,1),
+    ram int not null,
+    cpu int not null,
+    id_d int foreign key references deployments(id_d),
 )
 go 
-create table if not exists Repos ( 
-    id int primary key auto_increment,
+create table  Repos ( 
+    id_r int primary key identity(1,1) ,
     [name] varchar, 
     full_name varchar, 
     html_url varchar, 
-    emai varchar constrain fk_email references Users(email), 
+    email_user  varchar(255) foreign key references Users(email_user), 
 )
 
-
-create table  if not exists  import_card (
-    id int primary key auto_increment,
+go
+create table  import_card (
+    id_ic int primary key identity(1,1),
     repo_name varchar(255) not null,
     repo_full_name varchar(255) not null,
     repo_url varchar(255) not null,
     branch_name varchar(255) not null,
     created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp on update current_timestamp,
-    unique key unique_repo (user_email, repo_full_name),
-    index (user_email, repo_full_name),
-    user_email varchar(255) constrain fk_user_email references USers(email),
+    updated_at datetime default current_timestamp ,
+     user_email varchar(255) foreign key references Users(email_user),
 )
+go 
+create table  info_server ( 
+    server_ip varchar(50) primary key,
+    host varchar(255),
+    server_name varchar(255) not null,
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp ,
+    health_status varchar(50) not null,
+)
+create nonclustered index idx_offline 
+on info_server(health_status) 
+where health_status = 'offline';
+
+create nonclustered index idx_ip_status 
+on info_server(server_ip, health_status);
+
+go
+create table cpu_server (
+ us int ,
+sy int ,
+ni int , 
+id int  , 
+wa int ,
+hi int , 
+si int ,
+st int ,
+create_at datetime default current_timestamp,
+server_ip varchar(50) foreign key references info_server(server_ip)
+on delete cascade
+on update cascade
+)
+go
+create table memory_server ( 
+total int ,
+used int ,
+free int , 
+shared int,
+buff_cache int ,
+available int,
+server_ip varchar(50) foreign key references info_server(server_ip)
+on delete cascade
+on update cascade
+)
+
+go 
+create table disk_server ( 
+ getParmasDisk varchar(255), 
+size float , 
+used float, 
+avail float,
+usePercent float,
+mountpoint varchar(255),
+server_ip varchar(50) foreign key references info_server(server_ip)
+on delete cascade
+on update cascade
+) 
+go
+create table net_server ( 
+receive_KBps float,
+transmit_KBps float,
+server_ip varchar(50) foreign key references info_server(server_ip)
+on delete cascade
+on update cascade
+)
+
+go
+create table logged_server ( 
+username varchar(20),
+terminal varchar(20),
+ip_addres varchar(20), 
+time_log datetime ,
+action varchar(255),
+server_ip  varchar(50) foreign key references  info_server(server_ip)
+on delete cascade
+on update cascade
+
+)
+
+select *  from info_server
+insert into info_server(server_ip, server_name, host, health_status) values (
+'1.2.22.2', 'ubuntu', 'tente.sn'  'online'
+)
+
+create table  info_server ( 
+    server_ip varchar(50) primary key,
+    host varchar(255),
+    server_name varchar(255) not null,
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp ,
+    health_status varchar(50) not null,
+)
+
+
+
+
+
+
+
+
+
+
+ 
