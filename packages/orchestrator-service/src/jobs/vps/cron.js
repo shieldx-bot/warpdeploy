dabb0556575a3bbc67223fd80d9e1930f3fb48a5
Nguyen Van Anh <anhnv.24810310060@epu.edu.vn>
2025-11-06 20:37:38 +0700

    Remove unused VPS cron job file
    Deleted cron.js from the VPS jobs directory as it was not being used. This helps clean up the codebase and remove unnecessary files.

--- packages/orchestrator-service/src/jobs/vps/cron.js
+++
@@ -1,9 +0,0 @@
-const cron = require("node-cron");
-const { connectSSH } = require("../vps/ssh");
-
-
-cron.schedule("*/10 * * * * * ",async(req,res)=> {
-    console.log("Cron job started: Checking VPS status...");
-    
-    
-})
