97f255d3a0374fec3155d25e68561191f8696f6d
shieldx <anhnv.24810310060@epu.edu.vn>
2025-11-09 07:04:33 +0700

    chore: remove type.ts file from orchestrator-service

--- packages/orchestrator-service/src/types/type.ts
+++
@@ -1,9 +0,0 @@
-export interface metrix_vps { 
-    cpu: Record<string, number>;
-    mem: Record<string, number>;
-    disk: Array<any>;
-    net: Record<string, number>;
-    logged: Array<any>;
-}
-
-
