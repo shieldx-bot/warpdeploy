9210c6e16e18a0dd47c8a1c62954f7fb3e7d6eb7
shieldx <anhnv.24810310060@epu.edu.vn>
2025-11-09 07:01:11 +0700

    feat: add setUpNamespace function for managing Kubernetes namespaces

---
+++ packages/data-plane-service/src/k8s/setup/queryK8s.ts
@@ -0,0 +1,36 @@
+import * as  k8s from '@kubernetes/client-node';
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+
+export const setUpNamespace = async (namespace: any) => {
+
+    try {
+        await k8sApi.readNamespace(namespace);
+        return 'Namespace already exists';
+    } catch (error) {
+        try {
+            const ns = {
+                metadata: {
+                    name: namespace
+                }
+            }
+            const res = await k8sApi.createNamespace(ns as k8s.V1Namespace);
+            if (!res) {
+                return 'Failed to create namespace';
+            } else {
+                return 'Namespace created successfully';
+            }
+        } catch (err) {
+            return `Error creating namespace: ${err}`;
+        }
+
+    }
+
+
+
+}
+
