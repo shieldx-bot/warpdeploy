54832c6136cb01cf9d8f297f1da5032feeb44d98
shieldx <anhnv.24810310060@epu.edu.vn>
2025-11-30 21:43:28 +0700

    fix(database-service): correct users table creation and drop statement in schema.sql

--- services/database-service/src/schema.sql
+++ services/database-service/src/schema.sql
@@ -25,14 +25,33 @@ create table users (
     createdat datetime2 not null default sysdatetime ()
 );
 
-end if not exists (
+end 
+if not exists (select 1 from sys.tables  where name = 'users') 
+begin 
+	create table users ( 
+	email_user  char(30) primary key, 
+    username varchar not null,
+    password_hash varchar(255) not null,
+    role_at char(30) default 'users',
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp, 
+    github_username varchar(255),
+    github_token varchar(255),
+	)
+end
+select  * from users
+
+
+if not exists (
     select 1
     from sys.tables
     where
         name = 'cloud_server_user'
 ) begin
 create table cloud_server_user (
+  
     id int primary key identity (1, 1),
+    email_user char(30) foreign key references users(email_user), 
     clusterServer varchar(30),
     certificateAuthorityData varchar(248),
     clientCertificateData varchar(248),
@@ -42,5 +61,10 @@ create table cloud_server_user (
     clusterName varchar(30),
     contextName varchar(30)
 ) end
+
+drop table  users
+
+
+
 
 select * from sys.tables t
